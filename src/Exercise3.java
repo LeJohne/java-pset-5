d31eb1d09759da17d3ea9a33a35a204b2d606b00
LeJohne <johngoy11024@gmail.com>
2020-10-11 17:54:54 -0400

    exercise 3 - done

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter three integers.");
+
+        System.out.print("\nEnter integer: ");
+        int response1 = in.nextInt();
+        System.out.print("Enter integer: ");
+        int response2 = in.nextInt();
+        System.out.print("Enter integer: ");
+        int response3 = in.nextInt();
+
+        if((response1 < response2) && (response2 < response3)){
+            System.out.println("\nStrictly increasing.");
+        } else if((response1 < response2) && (response2 == response3 ) || ((response2 < response3) && (response1 ==response2))){
+            System.out.println("\nIncreasing.");
+        } else if ((response1 == response2) && (response2 == response3)){
+            System.out.println("\nSame.");
+        } else if((response1 > response2) && (response2 > response3)){
+            System.out.println("\nStrictly decreasing.");
+        } else if((response1 > response2) && (response2 == response3 ) || ((response2 > response3) && (response1 ==response2))){
+            System.out.println("\nDecreasing.");
+        } else {
+            System.out.println("\nUnordered.");
+        }
+
+        in.close();
+    }
 }
